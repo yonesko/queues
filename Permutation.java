733492b5d989a080826a61f2fa69cb5010018a1c
glebio <yonesko@gmail.com>
2019-09-09 23:56:56 +0300

    Fix bug

--- Permutation.java
+++ Permutation.java
@@ -10,11 +10,11 @@ public class Permutation {
     public static void main(String[] args) {
         int k = Integer.parseInt(args[0]);
         RandomizedQueue<String> queue = new RandomizedQueue<>();
-        for (int i = 0; i < k; i++) {
-            queue.enqueue(StdIn.readString());
+        for (String s : StdIn.readStrings()) {
+            queue.enqueue(s);
         }
-        for (String s : queue) {
-            System.out.println(s);
+        for (int i = 0; i < k; i++) {
+            System.out.println(queue.dequeue());
         }
     }
 }
