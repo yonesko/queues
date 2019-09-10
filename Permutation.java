5a9888aa7b82a0a35d885c591af60bc927cc61c8
glebio <yonesko@gmail.com>
2019-09-10 20:01:33 +0300

    Fix Permutation

--- Permutation.java
+++ Permutation.java
@@ -9,15 +9,17 @@ import edu.princeton.cs.algs4.StdIn;
 public class Permutation {
     public static void main(String[] args) {
         int k = Integer.parseInt(args[0]);
+        int printed = 0;
         RandomizedQueue<String> queue = new RandomizedQueue<>();
-        while (!StdIn.isEmpty()) {
+        while (!StdIn.isEmpty() && printed < k) {
             if (queue.size() == k) {
                 System.out.println(queue.dequeue());
+                printed++;
             }
             queue.enqueue(StdIn.readString());
         }
-        for (String s : queue) {
-            System.out.println(s);
+        for (int i = 0; i < k - printed; i++) {
+            System.out.println(queue.dequeue());
         }
     }
 }
